8f3d9e221499eca7416208c0d009f5223c3ded48
JuanPablo <jpabloaj@gmail.com>
2014-12-28 01:27:24 -0300

    node-slack-cli , first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# node-slack-cli
+
+Send messages to [slack.com](https://slack.com/) from command line with nodejs.
+
+## Install
+
+    npm install node-slack-cli -g
+
+## Environment variables
+
+To send to [slack.com](https://slack.com/) you need set the SLACK TOKEN as environment variable
+
+    export SLACK_TOKEN="slack-token"
+
+## Usage
+
+    slack -c slackChannelName -m "message to send"
+
---
+++ bin/slack.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var Slack = require('slack-node');
+var argv = require('minimist')(process.argv.slice(2));
+
+var channelName = argv.c;
+var message = argv.m;
+var slackToken = process.env.SLACK_TOKEN;
+
+if ( slackToken === undefined ) {
+  console.log('undefined SLACK_TOKEN as Env Variable');
+  process.exit(1);
+}
+
+if ( typeof(channelName) !== 'string' ){
+  console.log('undefined channel argv: -c');
+  process.exit(1);
+}
+
+if ( typeof(message) !== 'string' ){
+  console.log('undefined message argv: -m');
+  process.exit(1);
+}
+
+slack = new Slack(process.env.SLACK_TOKEN);
+slack.api(
+  'chat.postMessage', {text:message, channel:'#'+channelName},
+  function(){}
+);
